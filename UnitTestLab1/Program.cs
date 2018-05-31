68245a6177fcb05304a15d5007e179812ba408b7
Molyn Eriksson <molyneriksson@gmail.com>
2018-05-31 13:28:13 +0200

    Add project files.

---
+++ UnitTestLab1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UnitTestLab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
