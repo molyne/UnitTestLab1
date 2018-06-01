9d29b4a2c3e2f980f7886d78040ffce4cd070c99
Molyn Eriksson <molyneriksson@gmail.com>
2018-06-01 13:39:40 +0200

    cleaned code

--- UnitTestLab1/Account.cs
+++ UnitTestLab1/Account.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace UnitTestLab1
 {
-   public class Account
+    public class Account
     {
         public double Balance { get; private set; }
         // TODO  public double InterestRate { get; set; }
@@ -16,13 +16,13 @@ namespace UnitTestLab1
 
         public void Deposit(double amount)
         {
-            if (double.IsPositiveInfinity(amount)||double.IsNegativeInfinity(amount))
+            if (double.IsPositiveInfinity(amount) || double.IsNegativeInfinity(amount))
             {
                 throw new Exception("Exceeded infinity limit.");
             }
             else if (double.IsNaN(amount))
             {
-                throw new Exception("Amount must be a number");           
+                throw new Exception("Amount must be a number");
             }
             else
             {
@@ -31,6 +31,7 @@ namespace UnitTestLab1
         }
         public void Withdraw(double amount)
         {
+
             if (this.Balance <= 0)
             {
                 throw new Exception("Could not withdraw when balance under 0.");
@@ -48,7 +49,14 @@ namespace UnitTestLab1
         }
         public bool Transfer(Account target, double amount)
         {
-            if (double.IsNaN(amount))
+            bool successfullTransfer = false;
+
+            if (this == target)
+            {
+                throw new Exception("Can't transfer money to the same account.");
+            }
+
+            else if (double.IsNaN(amount))
             {
                 throw new Exception("The amount is not a number");
             }
@@ -56,19 +64,19 @@ namespace UnitTestLab1
             {
                 throw new Exception("The amount exceeded the infinity limit");
             }
-            else if (this == target)
-            {
-                throw new Exception("Can't transfer money to the same account.");
-            }
+
             else
             {
-                target.Balance = target.Balance + amount;
+                target.Deposit(amount);
 
-                this.Balance = this.Balance - amount;
+                this.Withdraw(amount);
+
+                successfullTransfer = true;
+
+                return successfullTransfer;
             }
 
 
-            return true;
         }
         //public double CalculateInterest()
         //{
